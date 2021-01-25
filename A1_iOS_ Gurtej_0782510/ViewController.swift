309728b5df56f187160f9156306eec33268a2cc6
Tej <gurtej1992@gmail.com>
2021-01-25 13:01:12 -0500

    Initial Commit

---
+++ A1_iOS_ Gurtej_0782510/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_iOS_ Gurtej_0782510
+//
+//  Created by Tej on 25/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
